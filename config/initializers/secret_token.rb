6a52db56e6a401d375242666a6c48d1933adbac6
Stash Rowe <Stash@Stashs-MacBook-Pro.local>
2014-07-14 23:15:40 +1000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '64b03e50b82306e2b72b79fe2bc4ed3336cf6d625ea81cfac7fd2289ddaa00cd8e5cb0135d5f549b16643b5dd6ff5d327f6f157daf4ab1d3d83f670b7116e5c2'
